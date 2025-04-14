237af0c10f15f77331fcb2ddf7b7dfa63f82ef52
Jacob <jacvn95@gmail.com>
2025-04-14 16:36:29 -0500

    add cancellationtoken

--- Infrastructure.Messaging/Publisher/EventPublisher.cs
+++ Infrastructure.Messaging/Publisher/EventPublisher.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,25 @@ namespace Infrastructure.Messaging.Publisher
     public class EventPublisher : IEventPublisher
     {
         private readonly IPublishEndpoint _publishEndpoint;
+        private readonly ILogger<EventPublisher> _logger;
 
-        public EventPublisher(IPublishEndpoint publishEndpoint)
+        public EventPublisher(IPublishEndpoint publishEndpoint, ILogger<EventPublisher> logger)
         {
             _publishEndpoint = publishEndpoint;
+            _logger = logger;
         }
 
         public async Task Publish<T>(T message) where T : class
         {
-            await _publishEndpoint.Publish(message);
+            try
+            {
+                var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+                await _publishEndpoint.Publish(message, cts.Token);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al publicar el mensaje de tipo {EventType}", typeof(T).Name);
+            }
         }
     }
 }
